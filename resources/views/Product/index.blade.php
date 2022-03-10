75a5bb44a99f46239d82c58e80dec155c00cfba9
unknown <ziraouimohammed3@gmail.com>
2022-03-10 12:31:05 +0100

    Add Fonts awesome in views

--- resources/views/Product/index.blade.php
+++ resources/views/Product/index.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">
                     <h4 class="d-inline">Products</h4>
                     @can('add products')
-                        <a class="d-inline" style="float:right" href="{{route("products.new")}}">New</a>
+                        <a class="d-inline" style="float:right" href="{{route("products.new")}}"><i class="fa-solid fa-circle-plus"></i></a>
                     @endcan
                 </div>
 
@@ -16,9 +16,9 @@
                     <ul>
                         @foreach ($products as $product)
                             <li>{{$product->name}}
-                                <a style="float:right" class="text-success" href="{{route("products.show",Crypt::encrypt($product->id))}}">show</a>
+                                <a style="float:right" class="text-success" href="{{route("products.show",Crypt::encrypt($product->id))}}"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
                                 @role("admin|writer")
-                                    <a style="float:right;margin-right:4px" href="{{route("products.edit",Crypt::encrypt($product->id))}}">Edit</a>
+                                    <a style="float:right;margin-right:4px" href="{{route("products.edit",Crypt::encrypt($product->id))}}"><i class="fa-solid fa-pen-to-square"></i></a>
                                 @endrole
                             </li>
                         @endforeach
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <script src="https://kit.fontawesome.com/2514aace29.js" crossorigin="anonymous"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
